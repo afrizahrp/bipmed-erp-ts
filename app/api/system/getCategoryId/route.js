9bece7778a20797dd91c831246726d48b9eea05e
AFRIZAHRP <afriza.hrp@gmail.com>
2024-06-18 22:01:36 +0700

    add getCategoryId js routes

--- app/api/system/getCategoryId/route.js
+++ app/api/system/getCategoryId/route.js
@@ -1,25 +1,16 @@
-import { prisma } from '@/lib/client';
+const { prisma } = require('@/lib/client');
 
-interface QueryResult {
-  doc_id: string;
-}
-
-async function getCategoryIdxxxx(
-  companyId: string,
-  branchId: string,
-  categoryType: string,
-  userId: string
-): Promise<string> {
+async function getCategoryId(companyId, branchId, categoryType, userId) {
   try {
-    const result: QueryResult[] =
+    const result =
       await prisma.$queryRaw`DECLARE @rt_doc_id VARCHAR(20); EXEC get_CategoryId ${companyId}, ${branchId},  ${categoryType},  ${userId}, @rt_doc_id OUTPUT; SELECT @rt_doc_id as doc_id;`;
     const docId = result[0].doc_id;
     return docId;
   } catch (e) {
     console.error(e);
-
     throw new Error('Something went wrong while trying to get document ID');
   }
 }
 
-export default getCategoryIdxxxx;
+export default getCategoryId;
+// module.exports = getCategoryId;
