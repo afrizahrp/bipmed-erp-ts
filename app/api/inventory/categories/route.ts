e695c74ecbed3adb64fecb68815fedfd70feeefc
afrizaharahap <Afriza.harahap@gmail.com>
2024-06-20 16:08:24 +0700

    update get id

--- app/api/inventory/categories/route.ts
+++ app/api/inventory/categories/route.ts
@@ -1,9 +1,11 @@
 import { prisma } from '@/lib/client';
 import { authOptions } from '@/lib/auth';
 import { NextRequest, NextResponse } from 'next/server';
-// import getCategoryId from '../../system/getCategoryId/route';
-
 import { getServerSession } from 'next-auth';
+
+interface QueryResult {
+  doc_id: string;
+}
 
 export async function GET(request: NextRequest) {
   try {
@@ -24,62 +26,82 @@ export async function GET(request: NextRequest) {
   }
 }
 
-// export async function POST(request: NextRequest) {
-//   try {
-//     const session = await getServerSession(authOptions);
-//     const company = session?.user?.company || '';
-//     const branch = session?.user?.branch || '';
-//     const username = session?.user?.name || '';
+async function getCategoryId(
+  companyId: string,
+  branchId: string,
+  categoryType: string,
+  userId: string
+): Promise<string> {
+  try {
+    const result: QueryResult[] =
+      await prisma.$queryRaw`DECLARE @rt_doc_id VARCHAR(20); EXEC get_CategoryId ${companyId}, ${branchId},  ${categoryType},  ${userId}, @rt_doc_id OUTPUT; SELECT @rt_doc_id as doc_id;`;
+    const docId = result[0].doc_id;
+    console.log('new category id is:', docId);
+    return docId;
+  } catch (e) {
+    console.error(e);
 
-//     const body = await request.json();
-//     const { type, name, remarks, imageURL, iStatus, iShowedStatus } = body as {
-//       type: string;
-//       name: string;
-//       remarks: string;
-//       imageURL: string;
-//       iStatus: boolean;
-//       iShowedStatus: boolean;
-//     };
+    throw new Error('Something went wrong while trying to get document ID');
+  }
+}
 
-//     const categoryType = type;
-//     const userId = username; //session.user.id // Use the user ID from the session
-//     const categoryId = await getCategoryId(
-//       company,
-//       branch,
-//       categoryType,
-//       userId
-//     );
 
-//     const newCategory = {
-//       type,
-//       name,
-//       id: categoryId,
-//       remarks,
-//       imageURL,
-//       iStatus,
-//       iShowedStatus,
-//       createdBy: username,
-//       updatedBy: username,
-//       createdAt: new Date(),
-//       updatedAt: new Date(),
-//       company: company,
-//       branch: branch,
-//     };
+export async function POST(request: NextRequest) {
+  try {
+    const session = await getServerSession(authOptions);
+    const company = session?.user?.company || '';
+    const branch = session?.user?.branch || '';
+    const username = session?.user?.name || '';
 
-//     const category = await prisma.categories.create({
-//       data: newCategory,
-//     });
+    const body = await request.json();
+    const { type, name, remarks, imageURL, iStatus, iShowedStatus } = body as {
+      type: string;
+      name: string;
+      remarks: string;
+      imageURL: string;
+      iStatus: boolean;
+      iShowedStatus: boolean;
+    };
 
-//     return NextResponse.json(category, { status: 201 });
-//   } catch (e) {
-//     console.error(e);
+    const categoryType = type;
+    const userId = username; //session.user.id // Use the user ID from the session
+    const categoryId = await getCategoryId(
+      company,
+      branch,
+      categoryType,
+      userId
+    );
 
-//     return NextResponse.json(
-//       {
-//         message: 'Something went wrong while trying to create new category',
-//         result: e,
-//       },
-//       { status: 500 }
-//     );
-//   }
-// }
+    const newCategory = {
+      type,
+      name,
+      id: categoryId,
+      remarks,
+      imageURL,
+      iStatus,
+      iShowedStatus,
+      createdBy: username,
+      updatedBy: username,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      company: company,
+      branch: branch,
+    };
+
+    const category = await prisma.categories.create({
+      data: newCategory,
+    });
+
+    return NextResponse.json(category, { status: 201 });
+  } catch (e) {
+    console.error(e);
+
+    return NextResponse.json(
+      {
+        message: 'Something went wrong while trying to create new category',
+        result: e,
+      },
+      { status: 500 }
+    );
+  }
+}
--- app/api/inventory/products/route.ts
+++ app/api/inventory/products/route.ts
@@ -4,6 +4,11 @@ import { authOptions } from '@/lib/auth';
 import { getServerSession } from 'next-auth';
 // import getProductId from '../../system/getProductId/route';
 
+
+interface QueryResult {
+  doc_id: string;
+}
+
 export async function GET(request: NextRequest) {
   try {
     const products = await prisma.products.findMany({
@@ -25,110 +30,129 @@ export async function GET(request: NextRequest) {
   }
 }
 
-// export async function POST(request: NextRequest) {
-//   try {
-//     const session = await getServerSession(authOptions);
-//     const company = session?.user?.company || '';
-//     const branch = session?.user?.branch || '';
-//     const userName = session?.user?.name || '';
 
-//     const body = await request.json();
-//     const {
-//       name,
-//       catalog_id,
-//       registered_id,
-//       category_id,
-//       subCategory_id,
-//       brand_id,
-//       uom_id,
-//       tkdn_pctg,
-//       bmp_pctg,
-//       ecatalog_URL,
-//       remarks,
-//       iStatus,
-//       images,
+async function getProductId(
+  companyId: string,
+  branchId: string,
+  prefixId: string,
+  userId: string
+): Promise<string> {
+  try {
+    const result: QueryResult[] =
+      await prisma.$queryRaw`DECLARE @rt_doc_id VARCHAR(20); EXEC get_ProductId ${companyId}, ${branchId},  ${prefixId},  ${userId}, @rt_doc_id OUTPUT; SELECT @rt_doc_id as doc_id;`;
+    const docId = result[0].doc_id;
+    return docId;
+  } catch (e) {
+    console.error(e);
 
-//       iShowedStatus,
-//       createdBy,
-//       updatedBy,
-//     } = body as {
-//       name: string;
-//       catalog_id: string;
-//       registered_id: string;
-//       category_id: string;
-//       subCategory_id: string;
-//       brand_id: string;
-//       uom_id: string;
-//       tkdn_pctg: number;
-//       bmp_pctg: number;
-//       ecatalog_URL: string;
-//       remarks: string;
-//       iStatus: string;
-//       images: { imageURL: string }[];
+    throw new Error('Something went wrong while trying to get document ID');
+  }
+}
 
-//       iShowedStatus: number;
-//       createdBy: string;
-//       created_at: string;
-//       updatedBy: string;
-//       updated_at: string;
-//     };
+export async function POST(request: NextRequest) {
+  try {
+    const session = await getServerSession(authOptions);
+    const company = session?.user?.company || '';
+    const branch = session?.user?.branch || '';
+    const userName = session?.user?.name || '';
 
-//     const userId = userName;
-//     const productId = await getProductId(company, branch, category_id, userId);
+    const body = await request.json();
+    const {
+      name,
+      catalog_id,
+      registered_id,
+      category_id,
+      subCategory_id,
+      brand_id,
+      uom_id,
+      tkdn_pctg,
+      bmp_pctg,
+      ecatalog_URL,
+      remarks,
+      iStatus,
+      images,
 
-//     const newProduct = {
-//       id: productId,
-//       name,
-//       catalog_id,
-//       registered_id,
-//       category_id,
-//       subCategory_id,
-//       brand_id,
-//       uom_id,
-//       tkdn_pctg,
-//       bmp_pctg,
-//       ecatalog_URL,
-//       remarks,
-//       iStatus,
-//       iShowedStatus,
+      iShowedStatus,
+      createdBy,
+      updatedBy,
+    } = body as {
+      name: string;
+      catalog_id: string;
+      registered_id: string;
+      category_id: string;
+      subCategory_id: string;
+      brand_id: string;
+      uom_id: string;
+      tkdn_pctg: number;
+      bmp_pctg: number;
+      ecatalog_URL: string;
+      remarks: string;
+      iStatus: string;
+      images: { imageURL: string }[];
 
-//       images: {
-//         deleteMany: {},
-//       },
-//       createdBy: userName,
-//       updatedBy: userName,
-//       createdAt: new Date(),
-//       updatedAt: new Date(),
-//       company: company,
-//       branch: branch,
-//     };
+      iShowedStatus: number;
+      createdBy: string;
+      created_at: string;
+      updatedBy: string;
+      updated_at: string;
+    };
 
-//     const product = await prisma.products.create({
-//       data: {
-//         ...newProduct,
-//         images: {
-//           createMany: {
-//             data: images.map((image: { imageURL: string }) => ({
-//               imageURL: image.imageURL,
-//             })),
-//           },
-//         },
-//       },
-//       // newProduct: {
-//       //   images: {
-//       //     createMany: {
-//       //       newProduct: [...images.map((image: { imageURL: string }) => image)],
-//       //     },
-//       //   },
-//       // },
-//     });
+    const userId = userName;
+    const productId = await getProductId(company, branch, category_id, userId);
 
-//     return NextResponse.json(product, { status: 201 });
-//   } catch (e) {
-//     console.error(e);
-//     return NextResponse.json(
-//       { error: 'Something went wrong' },
-//       { status: 500 }
-//     );
-//   }
-// }
+    const newProduct = {
+      id: productId,
+      name,
+      catalog_id,
+      registered_id,
+      category_id,
+      subCategory_id,
+      brand_id,
+      uom_id,
+      tkdn_pctg,
+      bmp_pctg,
+      ecatalog_URL,
+      remarks,
+      iStatus,
+      iShowedStatus,
+
+      images: {
+        deleteMany: {},
+      },
+      createdBy: userName,
+      updatedBy: userName,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      company: company,
+      branch: branch,
+    };
+
+    const product = await prisma.products.create({
+      data: {
+        ...newProduct,
+        images: {
+          createMany: {
+            data: images.map((image: { imageURL: string }) => ({
+              imageURL: image.imageURL,
+            })),
+          },
+        },
+      },
+      // newProduct: {
+      //   images: {
+      //     createMany: {
+      //       newProduct: [...images.map((image: { imageURL: string }) => image)],
+      //     },
+      //   },
+      // },
+    });
+
+    return NextResponse.json(product, { status: 201 });
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      { error: 'Something went wrong' },
+      { status: 500 }
+    );
+  }
+}
