a1200cf99f28c6b17cd54be4359ebfccfb461ddc
afrizaharahap <Afriza.harahap@gmail.com>
2024-06-20 11:25:34 +0700

    update

--- components/ui/data-table-row-actions.tsx
+++ components/ui/data-table-row-actions.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { DotsHorizontalIcon } from '@radix-ui/react-icons';
+import { MoreHorizontal } from 'lucide-react';
 import { Row } from '@tanstack/react-table';
 
 import { Button } from '@/components/ui/button';
@@ -19,16 +19,16 @@ import {
 } from '@/components/ui/dropdown-menu';
 
 import { labels } from '@/data/data';
-import { productSchema } from '@/data/schema';
 
-interface DataTableRowActionsProps<TData> {
-  row: Row<TData>;
+// import { labels } from "../data/data";
+import { taskSchema } from '@/data/schema';
+
+interface DataTableRowActionsProps {
+  row: Row<any>;
 }
 
-export function DataTableRowActions<TData>({
-  row,
-}: DataTableRowActionsProps<TData>) {
-  const task = productSchema.parse(row.original);
+export function DataTableRowActions({ row }: DataTableRowActionsProps) {
+  const task = taskSchema.parse(row.original);
 
   return (
     <DropdownMenu>
@@ -37,7 +37,7 @@ export function DataTableRowActions<TData>({
           variant='ghost'
           className='flex h-8 w-8 p-0 data-[state=open]:bg-muted'
         >
-          <DotsHorizontalIcon className='h-4 w-4' />
+          <MoreHorizontal className='h-4 w-4' />
           <span className='sr-only'>Open menu</span>
         </Button>
       </DropdownMenuTrigger>
@@ -49,7 +49,7 @@ export function DataTableRowActions<TData>({
         <DropdownMenuSub>
           <DropdownMenuSubTrigger>Labels</DropdownMenuSubTrigger>
           <DropdownMenuSubContent>
-            <DropdownMenuRadioGroup value={task.name}>
+            <DropdownMenuRadioGroup value={task.label}>
               {labels.map((label) => (
                 <DropdownMenuRadioItem key={label.value} value={label.value}>
                   {label.label}
