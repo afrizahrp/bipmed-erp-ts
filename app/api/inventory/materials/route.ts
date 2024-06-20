222056128e561caa937af4654909afda71d9fb18
afrizaharahap <Afriza.harahap@gmail.com>
2024-06-20 16:04:39 +0700

    compiled is successed

--- app/api/inventory/materials/route.ts
+++ app/api/inventory/materials/route.ts
@@ -4,6 +4,10 @@ import { authOptions } from '@/lib/auth';
 import { getServerSession } from 'next-auth';
 // import getMaterialId from './../../system/getMaterialId/route';
 
+interface QueryResult {
+  doc_id: string;
+}
+
 export async function GET(request: NextRequest) {
   try {
     const products = await prisma.products.findMany({
@@ -25,97 +29,110 @@ export async function GET(request: NextRequest) {
   }
 }
 
-// export async function POST(request: NextRequest) {
-//   try {
-//     const session = await getServerSession(authOptions);
-//     const company = session?.user?.company || '';
-//     const branch = session?.user?.branch || '';
-//     const userName = session?.user?.name || '';
+async function getMaterialId(
+  companyId: string,
+  branchId: string,
+  prefixId: string,
+  userId: string
+): Promise<string> {
+  try {
+    const result: QueryResult[] =
+      await prisma.$queryRaw`DECLARE @rt_doc_id VARCHAR(20); EXEC get_MaterialId ${companyId}, ${branchId},  ${prefixId},  ${userId}, @rt_doc_id OUTPUT; SELECT @rt_doc_id as doc_id;`;
+    const docId = result[0].doc_id;
+    return docId;
+  } catch (e) {
+    console.error(e);
 
-//     const body = await request.json();
-//     const {
-//       name,
-//       catalog_id,
-//       registered_id,
-//       category_id,
-//       subCategory_id,
-//       brand_id,
-//       uom_id,
-//       tkdn_pctg,
-//       bmp_pctg,
-//       ecatalog_URL,
-//       remarks,
-//       iStatus,
-//       isMaterial,
-//       iShowedStatus,
-//       createdBy,
-//       updatedBy,
-//     } = body as {
-//       name: string;
-//       catalog_id: string;
-//       registered_id: string;
-//       category_id: string;
-//       subCategory_id: string;
-//       brand_id: string;
-//       uom_id: string;
-//       tkdn_pctg: number;
-//       bmp_pctg: number;
-//       ecatalog_URL: string;
-//       remarks: string;
-//       iStatus: string;
-//       isMaterial: boolean;
-//       iShowedStatus: number;
-//       createdBy: string;
-//       created_at: string;
-//       updatedBy: string;
-//       updated_at: string;
-//     };
+    throw new Error('Something went wrong while trying to get document ID');
+  }
+}
 
-//     const userId = userName;
+export async function POST(request: NextRequest) {
+  try {
+    const session = await getServerSession(authOptions);
+    const company = session?.user?.company || '';
+    const branch = session?.user?.branch || '';
+    const userName = session?.user?.name || '';
 
-//     const materialId = await getMaterialId(
-//       company,
-//       branch,
-//       category_id,
-//       userId
-//     );
+    const body = await request.json();
+    const {
+      name,
+      catalog_id,
+      registered_id,
+      category_id,
+      subCategory_id,
+      brand_id,
+      uom_id,
+      tkdn_pctg,
+      bmp_pctg,
+      ecatalog_URL,
+      remarks,
+      iStatus,
+      slug,
+      isMaterial,
+      iShowedStatus,
+    } = body as {
+      name: string;
+      catalog_id: string;
+      registered_id: string;
+      category_id: string;
+      subCategory_id: string;
+      brand_id: string;
+      uom_id: string;
+      tkdn_pctg: number;
+      bmp_pctg: number;
+      ecatalog_URL: string;
+      iStatus: boolean;
+      remarks: string;
+      slug: string;
+      isMaterial: boolean;
+      iShowedStatus: boolean;
+    };
 
-//     const newMaterial = {
-//       id: materialId,
-//       name,
-//       catalog_id,
-//       registered_id,
-//       category_id,
-//       subCategory_id,
-//       brand_id,
-//       uom_id,
-//       tkdn_pctg,
-//       bmp_pctg,
-//       ecatalog_URL,
-//       remarks,
-//       iStatus,
-//       iShowedStatus,
-//       isMaterial: true,
-//       createdBy: userName,
-//       updatedBy: userName,
-//       createdAt: new Date(),
-//       updatedAt: new Date(),
-//       company: company,
-//       branch: branch,
-//     };
+    const userId = userName;
 
-//     const material = await prisma.products.create({
-//       data: {
-//         ...newMaterial,
-//       },
-//     });
+    const materialId = await getMaterialId(
+      company,
+      branch,
+      category_id,
+      userId
+    );
 
-//     return NextResponse.json(material, { status: 201 });
-//   } catch (e) {
-//     console.error(e);
-//     return NextResponse.json(
-//       { error: 'Something went wrong while trying to create new material' },
-//       { status: 500 }
-//     );
-//   }
-// }
+    const newMaterial = {
+      id: materialId,
+      name,
+      catalog_id,
+      registered_id,
+      category_id,
+      subCategory_id,
+      brand_id,
+      uom_id,
+      tkdn_pctg,
+      bmp_pctg,
+      ecatalog_URL,
+      iStatus,
+      remarks,
+      slug: '',
+      isMaterial: true,
+      iShowedStatus: false,
+      createdBy: userName,
+      updatedBy: userName,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      company: company,
+      branch: branch,
+    };
+
+    const material = await prisma.products.create({
+      data: newMaterial,
+    });
+
+    return NextResponse.json(material, { status: 201 });
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      { error: 'Something went wrong while trying to create new material' },
+      { status: 500 }
+    );
+  }
+}
