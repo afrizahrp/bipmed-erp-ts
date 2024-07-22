52d7094fc57e7a9ce56b29b1e5954844a78a8f20
AFRIZAHRP <afriza.hrp@gmail.com>
2024-07-22 17:59:44 +0700

    update next-public-secret

--- lib/auth.ts
+++ lib/auth.ts
@@ -52,7 +52,7 @@ export const authOptions: NextAuthOptions = {
     strategy: 'jwt',
   },
   jwt: {
-    secret: process.env.NEXT_PUBLIC_SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
   },
 
   callbacks: {
