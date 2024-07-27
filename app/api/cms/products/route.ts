372fad0e04c276dbf22c5d20ae8e82c05da79fb4
AFRIZAHRP <afriza.hrp@gmail.com>
2024-07-28 00:25:04 +0700

    create public products endpoint in cms module

---
+++ app/api/cms/products/route.ts
@@ -0,0 +1,44 @@
+import { prisma } from '@/lib/client';
+import { NextRequest, NextResponse } from 'next/server';
+import { authOptions } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
+
+export async function GET(request: NextRequest) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const iShowedStatus = searchParams.get('iShowedStatus');
+    const products = await prisma.products.findMany({
+      where: {
+        isMaterial: false,
+        iShowedStatus: iShowedStatus === 'true' ? true : false,
+      },
+      include: {
+        images: true,
+        specs: true,
+        descriptions: true,
+        showStatus: true,
+      },
+
+      orderBy: {
+        updatedAt: 'desc',
+      },
+    });
+    const productsWithPrimaryImage = products.map((product) => {
+      const primaryImages = product.images.filter((image) => image.isPrimary);
+      const primaryImageURL =
+        primaryImages.length > 0 ? primaryImages[0].imageURL : null;
+      return {
+        ...product,
+        primaryImageURL,
+      };
+    });
+
+    return NextResponse.json(productsWithPrimaryImage);
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json(
+      { error: 'Something went wrong' },
+      { status: 500 }
+    );
+  }
+}
