d54a1208c8314569135379db8c45caf30deac695
AFRIZAHRP <afriza.hrp@gmail.com>
2024-07-29 23:43:44 +0700

    remove unused library

--- app/[lang]/error.tsx
+++ app/[lang]/error.tsx
@@ -1,24 +1,26 @@
-"use client"; // Error components must be Client Components
+'use client'; // Error components must be Client Components
 
-import { useEffect } from "react";
-import { Button } from "@/components/ui/button";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { Info } from "lucide-react";
+import { useEffect } from 'react';
+import { Button } from '@/components/ui/button';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { Info } from 'lucide-react';
 
-export default function Error({ error, reset }: {
-  error: Error & { digest?: string }
-  reset: () => void
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
 }) {
   return (
-    <div className="space-y-4">
-      <Alert color="destructive" variant="soft">
-        <Info className="h-5 w-5" />
+    <div className='space-y-4'>
+      <Alert color='destructive' variant='soft'>
+        <Info className='h-5 w-5' />
         <AlertDescription>Something went wrong!</AlertDescription>
       </Alert>
-      <Button onClick={() => reset()} color="destructive" size="sm">
+      <Button onClick={() => reset()} color='destructive' size='sm'>
         Try again
       </Button>
     </div>
   );
 }
-
