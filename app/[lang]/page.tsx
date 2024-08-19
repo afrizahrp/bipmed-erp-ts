d56c1ff13fc24e91fcf2fe5fb532e81db15a111a
AFRIZAHRP <afriza.hrp@gmail.com>
2024-08-19 21:58:46 +0700

    add react-spinner and remove poppins from Home Page

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,10 +1,10 @@
-import { Poppins } from 'next/font/google';
 import { LoginForm } from '@/components/auth/login-form';
 
-const font = Poppins({
-  subsets: ['latin'],
-  weight: ['600'],
-});
+// import { Poppins } from 'next/font/google';
+// const font = Poppins({
+//   subsets: ['latin'],
+//   weight: ['600'],
+// });
 
 export default function Home() {
   return (
