8d758fffc7a1612371e5f56c250ba7b107d27b14
AFRIZAHRP <afriza.hrp@gmail.com>
2025-03-15 21:29:30 +0700

    update mail.ts

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,16 +2,16 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-// const getLocalizedDomain = (lang: string) => {
-//   const domain = process.env.NEXT_PUBLIC_APP_URL + '/[lang]';
-//   return domain.replace('[lang]', lang);
-// };
-
-
 const getLocalizedDomain = (lang: string) => {
   const domain = process.env.NEXT_PUBLIC_APP_URL + '/[lang]';
   return domain.replace('[lang]', lang);
 };
+
+
+// const getLocalizedDomain = (lang: string) => {
+//   const domain = process.env.NEXT_PUBLIC_APP_URL + '/[lang]';
+//   return domain.replace('[lang]', lang);
+// };
 
 export const sendTwoFactorTokenEmail = async (
   email: string,
@@ -48,9 +48,9 @@ export const sendVerificationEmail = async (
   token: string,
   lang: string
 ) => {
-  const domain = process.env.NEXT_PUBLIC_APP_URL
+  // const domain = process.env.NEXT_PUBLIC_APP_URL
   const localizedDomain = getLocalizedDomain(lang);
-   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
+  const confirmLink = `${localizedDomain}/auth/new-verification?token=${token}`;
 
   // const confirmLink = `/auth/new-verification?token=${token}`;
 
